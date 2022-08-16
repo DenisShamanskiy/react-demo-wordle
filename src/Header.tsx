f15b1d385d146032f1d07d32899f9ee3386c85bd
DenisShamanskiy <75094596+DenisShamanskiy@users.noreply.github.com>
2022-08-17 00:53:15 +0300

    Верстка Header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,10 @@
-// import React, { useEffect, useState } from 'react';
-// import './Header.css';
+// import React from 'react';
 
 const Header = () => {
 
   return (
-    <header className="w-full h-10 px-3 border-b border-gray-400">
-        <div className="inline-block align-bottom font-mono text-2xl leading-10 font-bold tracking-wide">Wordle</div>
+    <header className="h-10 sm:h-[65px] px-4 md:px-5 flex flex-row items-center justify-start flex-nowrap text-[color:var(--color-tone-1)] border-b border-[color:var(--color-tone-4)] box-content">
+        <div className=" grow-[2] font-bold text-[28px] md:text-[32px] lg:text-[36px] tracking-[0.01em] text-left md:text-center left-0 right-0 pointer-events-none relative box-border leading-none">Wordle</div>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "415px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+    },
     extend: {},
   },
   plugins: [],
