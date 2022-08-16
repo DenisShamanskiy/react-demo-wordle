21840c6a5b5f0c7336712a09d397c976fe89b592
DenisShamanskiy <75094596+DenisShamanskiy@users.noreply.github.com>
2022-08-16 22:37:05 +0300

    Базовая настройка. Добавлен “Tailwind CSS. Верстка доски

---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+// import React, { useEffect, useState } from 'react';
+// import './Header.css';
+
+const Header = () => {
+
+  return (
+    <header className="w-full h-10 px-3 border-b border-gray-400">
+        <div className="inline-block align-bottom font-mono text-2xl leading-10 font-bold tracking-wide">Wordle</div>
+    </header>
+  );
+}
+
+export default Header;
